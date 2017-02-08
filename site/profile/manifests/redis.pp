0470f173734daf6c77c383b4e92fc354f142e374
rrod157 <rustan.rodriguez@safeway.com>
2017-02-09 05:06:36 +0800

    Update redis.pp

--- site/profile/manifests/redis.pp
+++ site/profile/manifests/redis.pp
@@ -2,11 +2,10 @@ class profile::redis {
   # We'll require our epel class so that it is enforced first.
   # Don't forget to update that class before enforcing your code
   require profile::epel
-  #contain is necesary for fix lab bug
-  contain redis
   class { 'redis':
     # what parameter should we pass to set maxmemory to 10mb?
     maxmemory => '10mb'
   }
-  
+  #contain is necesary for fix lab bug
+  contain redis
 }
