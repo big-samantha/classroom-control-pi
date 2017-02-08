7c362173771dea5d6aeaa2ef54dfc9a1987d863a
00rellana <oscarxorellana@gmail.com>
2017-02-07 20:12:19 -0700

    Update site.pp
    commented out notify to reduce noise

--- manifests/site.pp
+++ manifests/site.pp
@@ -39,5 +39,5 @@ node orellana.puppetlabs.vm {
   # example code for the classroom
   include examples::puppetize
   
-  notify { "This will only be enforced on the Linux container.": }
+  #notify { "This will only be enforced on the Linux container.": }
 }
