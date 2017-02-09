843e962c21d03e75a9dfbb3f0c84cd8414dc3834
fponce26 <francis.ponce@safeway.com>
2017-02-10 03:50:52 +0800

    Update managed_user.pp

--- site/skeleton/manifests/managed_user.pp
+++ site/skeleton/manifests/managed_user.pp
@@ -1,70 +1,55 @@
 define skeleton::managed_user (
-  $password,
-  $home = undef,
-  # TODO: Add a password parameter
-  
+$password, # this was added
+$home = undef,
 ) {
-  if $home {
-    $homedir = $home
-  }
-  else {
-    $homedir = $osfamily ? {
-      'windows' => "C:/Users/${name}",
-      'RedHat'  => "/home/${name}",
-    }
-  }
-
-  if $osfamily == 'windows' {
-    # set resource defaults so the file properties are set appropriately
-    File {
-      owner => $name,
-      group => 'Administrators',
-      mode  => '0664',
-    }
-    User {
-      groups => ['Administrators', 'Users'],
-    }
-
-    # evaluated each time a powershell session starts
-    file { "${homedir}/Documents/WindowsPowerShell/profile.ps1":
-      ensure => file,
-      source => 'puppet:///modules/skeleton/profile.ps1',
-    }
-
-    file { "${homedir}/Documents/WindowsPowerShell":
-      ensure => directory,
-    }
-
-  }
-  else {
-    File {
-      owner => $name,
-      group => 'wheel',
-      mode  => '0644',
-    }
+if $home {
+$homedir = $home
+}
+else {
+$homedir = $osfamily ? {
+'windows' => "C:/Users/${name}",
+'RedHat' => "/home/${name}",
+}
+}
+if $osfamily == 'windows' {
+# set resource defaults so the file properties are set appropriately
+File {
+owner => $name,
+group => 'Administrators',
+mode => '0664',
+}
+User {
+groups => ['Administrators', 'Users'],
+}
+# evaluated each time a powershell session starts
+file { "${homedir}/Documents/WindowsPowerShell/profile.ps1":
+ensure => file,
+source => 'puppet:///modules/skeleton/profile.ps1',
+}
+file { "${homedir}/Documents/WindowsPowerShell":
+ensure => directory,
+}
+}
+else {
+File {
+owner => $name,
+group => 'wheel',
+mode => '0644',
+}
+# This resource was added. How did you know what variable to use here?
 file { "${homedir}/.bashrc":
 ensure => file,
 source => 'puppet:///modules/skeleton/bashrc',
 }
-    # TODO: Add a file resource to manage "${homedir}/.bashrc"
-
-  }
-
-  # Puppet will evaluate these resources in the proper order because it's smart
-  # and knows about dependencies between files and their owners
-
-  user { $name:
-    ensure     => present,
-    managehome => true,
-    # TODO: Pass the password parameter to this resource
-    password => $password,
-    file { "${homedir}/.bashrc":
-ensure => file,
-source => 'puppet:///modules/skeleton/bashrc',
 }
-  }
-
-  file { $homedir:
-    ensure => directory,
-  }
+# Puppet will evaluate these resources in the proper order because it's smart
+# and knows about dependencies between files and their owners
+user { $name:
+ensure => present,
+password => $password, # this was added
+managehome => true,
+}
+file { $homedir:
+ensure => directory,
+}
 }
