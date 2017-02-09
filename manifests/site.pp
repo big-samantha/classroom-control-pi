553fcd28cfd66793c27abadcc25d8062a5df79f9
rrod157 <rustan.rodriguez@safeway.com>
2017-02-10 07:48:33 +0800

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -36,4 +36,6 @@ node default {
 node rrod157.puppetlabs.vm {
   notify { "This will only be enforced on the Linux container.": }
   notify { "The primary disk is ${::disks['sda']['size']} in size.": }
+  $message = hiera('message')
+  notify { $message: }
 }
