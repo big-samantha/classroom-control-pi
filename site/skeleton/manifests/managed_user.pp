6ff7ed3121dc69b445a1c268e7ad45a7a34f7cde
00rellana <oscarxorellana@gmail.com>
2017-02-09 12:08:34 -0700

    Update managed_user.pp
    added  password and .bashrc

--- site/skeleton/manifests/managed_user.pp
+++ site/skeleton/manifests/managed_user.pp
@@ -1,6 +1,6 @@
 define skeleton::managed_user (
   $home = undef,
-  # TODO: Add a password parameter
+  $password,
   
 ) {
   if $home {
@@ -42,7 +42,10 @@ define skeleton::managed_user (
       mode  => '0644',
     }
 
-    # TODO: Add a file resource to manage "${homedir}/.bashrc"
+     file { "${homedir}/.bashrc":
+       ensure => file,
+       source => 'puppet:///modules/skeleton/bashrc',
+    }    
 
   }
 
@@ -52,7 +55,7 @@ define skeleton::managed_user (
   user { $name:
     ensure     => present,
     managehome => true,
-    # TODO: Pass the password parameter to this resource
+    password => $password,
     
   }
 
