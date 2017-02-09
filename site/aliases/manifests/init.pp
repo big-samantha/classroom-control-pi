fed694974e6ffd9b5b9f633fdb8b5f66883da6f6
00rellana <oscarxorellana@gmail.com>
2017-02-09 14:23:16 -0700

    Update init.pp
    added use of epp()  function

--- site/aliases/manifests/init.pp
+++ site/aliases/manifests/init.pp
@@ -1,17 +1,16 @@
 class aliases (
-    $admin = 'root',
+$admin = 'root',
 ) {
-    # uses $admin to build the aliases file
-    # TODO: Add the function call to generate text from a template
-    file { '/etc/aliases':
-        ensure  => file,
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0644',
-        content => '???', # What should go here?
-    }
-    exec { '/usr/bin/newaliases':
-        refreshonly => true,
-        subscribe   => File['/etc/aliases'],
-    }
+# uses $admin to build the aliases file
+file { '/etc/aliases':
+ensure => file,
+owner => 'root',
+group => 'root',
+mode => '0644',
+content => epp('aliases/aliases.epp', { admin => $admin }),
+}
+exec { '/usr/bin/newaliases':
+refreshonly => true,
+subscribe => File['/etc/aliases'],
+}
 }
