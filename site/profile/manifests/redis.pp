30a21a0066ad33b9835726992e661c1ad37f848d
rrod157 <rustan.rodriguez@safeway.com>
2017-02-10 04:13:46 +0800

    Update redis.pp

--- site/profile/manifests/redis.pp
+++ site/profile/manifests/redis.pp
@@ -1,6 +1,5 @@
 class profile::redis (
   $master = false,
-  {
  ){
   # We'll require our epel class so that it is enforced first.
   # Don't forget to update that class before enforcing your code
