a0f51b197fde4e85b1cd41805ab2cd44f7b3c06c
em2keerthi <keerthi.mallik@safeway.com>
2017-02-08 17:35:39 -0700

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,11 +1,4 @@
 class skeleton {
-  file { '/etc/skel':
-#    ensure => ???,   # what value should go here?
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
-  }
-  
   # add a resource to manage /etc/skel/.bashrc
 file { '/etc/skel/.bashrc':
 ensure => file,
