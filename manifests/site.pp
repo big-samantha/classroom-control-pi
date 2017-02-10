96841f817d0511e6f6bb493d2c768826f21fce1b
fponce26 <francis.ponce@safeway.com>
2017-02-10 08:21:48 +0800

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -25,7 +25,7 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  $message = hiera('hail heira')
+  $message = hiera('message')
   notify { $message: }
 
   # example code for the classroom
